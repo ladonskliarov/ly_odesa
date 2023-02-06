0ba655839b72ec6ebbd8697828abda1256698b9b
Ladon Skliarov <ladonskliarov@gmail.com>
2023-02-06 19:08:07 +0200

    Add props for states in AuthState

--- lib/domain/blocs/auth_bloc/auth_state.dart
+++ lib/domain/blocs/auth_bloc/auth_state.dart
@@ -20,12 +20,12 @@ class LoginLoadedState extends AuthState {
   const LoginLoadedState({required this.user});
 
   @override
-  List<Object?> get props => [];
+  List<Object?> get props => [user];
 }
 
 class LoginStateError extends AuthState {
   final Error error;
   const LoginStateError(this.error);
   @override
-  List<Object?> get props => [];
+  List<Object?> get props => [error];
 }
