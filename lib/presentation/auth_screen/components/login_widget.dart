05461963ddb7f67f32f8ef55efa7b3aa17145653
Ladon Skliarov <ladonskliarov@gmail.com>
2023-02-26 14:30:04 +0200

    Refactor AuthScreen. Add the validation for login and register fields

--- lib/presentation/auth_screen/components/login_widget.dart
+++ lib/presentation/auth_screen/components/login_widget.dart
@@ -15,6 +15,7 @@ class LoginWidget extends StatefulWidget {
 class _LoginWidgetState extends State<LoginWidget> {
   TextEditingController emailController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -22,93 +23,118 @@ class _LoginWidgetState extends State<LoginWidget> {
       create: (context) => FirebaseAuthService(),
       child: BlocProvider(
         create: (context) => AuthBloc(RepositoryProvider.of<FirebaseAuthService>(context)),
-        child: Column(
-            children: [
-              Expanded(
-                flex: 1,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    GestureDetector(
-                      onTap: () {
-                        Navigator.pushAndRemoveUntil(context, MaterialPageRoute(builder: (context) => const HomeScreen()), (route) => false);
-                        },
-                      child: SizedBox(width: 120, height: 120,
-                          child: Image.asset('assets/images/brand/ly_dark.png', fit: BoxFit.fill,)
-                      ),
-                    ),
-                    Padding(
-                        padding: const EdgeInsets.only(right: 20.0),
-                        child: GestureDetector(
-                            onTap: () {
-                              BlocProvider.of<AuthBloc>(context).add(const ChooseRegisterEvent());
-                            },
-                            child: const Text('Реєстрація', style: TextStyle(
-                                fontSize: 25, color: Colors.grey),))
-                    ),
-                  ],
-                ),
-              ),
-              Expanded(
-                flex: 5,
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(
-                  horizontal: 40.0, vertical: 10),
-                  child: SizedBox(
-                    height: double.infinity,
-                    width: double.infinity,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.start,
+        child: SafeArea(
+          child: Column(
+                children: [
+                  Expanded(
+                    flex: 1,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.center,
                       children: [
-                        const Text('Привіт!', style: TextStyle(
-                            fontSize: 25, fontWeight: FontWeight.w700),),
-                        const Text('Залогінься щоб продовжити',
-                          style: TextStyle(fontSize: 20),),
-                        TextFieldCustom(
-                          controller: emailController,
-                            icon: const Icon(Icons.account_circle_rounded, color: Colors.white, size: 20),
-                            hintText: 'Пошта'),
-                        TextFieldCustom(
-                          obscureText: true,
-                          controller: passwordController,
-                            icon: const Icon(Icons.lock_outline_rounded, color: Colors.white, size: 20,),
-                            hintText: 'Пароль'),
-                        Padding(
-                          padding: const EdgeInsets.only(top: 10.0, bottom: 5.0),
-                          child: DecoratedBox(
-                            decoration: BoxDecoration(color: Colors.white,
-                              borderRadius: BorderRadius.circular(20.0)),
-                                child: GestureDetector(
-                                  onTap: () async {
-                                    BlocProvider.of<AuthBloc>(context).add(
-                                        LoginEvent(email: emailController.text, password: passwordController.text)
-                                    );
-                                  },
-                                  child: const SizedBox(
-                                    height: 40,
-                                    width: double.infinity,
-                                      child: Align(
-                                        alignment: Alignment.center,
-                                          child: Text('Увійти', style: TextStyle(
-                                              color: Color(0xff1b1a1a),
-                                              fontSize: 20),
-                                          )
-                                      ),
-                              ),)
+                        GestureDetector(
+                          onTap: () {
+                            Navigator.pushAndRemoveUntil(context, MaterialPageRoute(builder: (context) => const HomeScreen()), (route) => false);
+                            },
+                          child: SizedBox(width: 120, height: 120,
+                              child: Image.asset('assets/images/brand/ly_dark.png', fit: BoxFit.fill,)
                           ),
                         ),
-                        const Text('Завдяки цьому кожне замовлення заповнюється автоматично*',
-                          style: TextStyle(fontSize: 15, color: Colors.grey),),
+                        Padding(
+                            padding: const EdgeInsets.only(right: 20.0),
+                            child: GestureDetector(
+                                onTap: () {
+                                  BlocProvider.of<AuthBloc>(context).add(const ChooseRegisterEvent());
+                                },
+                                child: const Text('Реєстрація', style: TextStyle(
+                                    fontSize: 25, color: Colors.grey),))
+                        ),
                       ],
                     ),
                   ),
-                ),
+                  Expanded(
+                    flex: 5,
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                      horizontal: 40.0, vertical: 10),
+                      child: SizedBox(
+                        height: double.infinity,
+                        width: double.infinity,
+                        child: Form(
+                          key: _formKey,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              const Text('Привіт!', style: TextStyle(
+                                  fontSize: 25, fontWeight: FontWeight.w700, color: Colors.white),),
+                              const Text('Залогінься щоб продовжити',
+                                style: TextStyle(fontSize: 20, color: Colors.white),),
+                              TextFieldCustom(
+                                validator: (value) {
+                                  if (value == null || value.isEmpty) {
+                                    return 'Здається тут немає тексту';
+                                  }
+                                  return null;
+                                },
+                                controller: emailController,
+                                  icon: const Icon(Icons.account_circle_rounded, color: Colors.white, size: 20),
+                                  hintText: 'Пошта'),
+                              TextFieldCustom(
+                                  validator: (value) {
+                                    if (value == null || value.isEmpty) {
+                                      return 'Здається тут немає тексту';
+                                    }
+                                    return null;
+                                  },
+                                obscureText: true,
+                                controller: passwordController,
+                                  icon: const Icon(Icons.lock_outline_rounded, color: Colors.white, size: 20,),
+                                  hintText: 'Пароль'),
+                              Padding(
+                                padding: const EdgeInsets.only(top: 10.0, bottom: 5.0),
+                                child: DecoratedBox(
+                                  decoration: BoxDecoration(color: Colors.white,
+                                    borderRadius: BorderRadius.circular(20.0)),
+                                      child: GestureDetector(
+                                        onTap: () {
+                                          if (_formKey.currentState!.validate()) {
+                                                BlocProvider.of<AuthBloc>(context).add(
+                                                    LoginEvent(email: emailController.text, password: passwordController.text)
+                                            );
+                                          } else {
+                                            ScaffoldMessenger.of(context).showSnackBar(
+                                              const SnackBar(
+                                                  backgroundColor: Colors.white,
+                                                  content: Text('О оу, перегляньте заповнення', style: TextStyle(color: Color(0xff1b1a1a)),)),
+                                            );
+                                          }
+                                        },
+                                        child: const SizedBox(
+                                          height: 40,
+                                          width: double.infinity,
+                                            child: Align(
+                                              alignment: Alignment.center,
+                                                child: Text('Увійти', style: TextStyle(
+                                                    color: Color(0xff1b1a1a),
+                                                    fontSize: 20),
+                                                )
+                                            ),
+                                    ),)
+                                ),
+                              ),
+                              const Text('Завдяки цьому кожне замовлення заповнюється автоматично*',
+                                style: TextStyle(fontSize: 15, color: Colors.grey),),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
               ),
-            ],
-          ),
-      ),
+        ),
+        ),
     );
   }
 }
